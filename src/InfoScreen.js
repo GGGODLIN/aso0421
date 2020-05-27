e7d21272e8b12b7c444dc36de20a5599884876c6
GGGODLIN <qwe70301@gmail.com>
2020-05-27 11:35:56 +0800

    0527_done

--- src/InfoScreen.js
+++ src/InfoScreen.js
@@ -44,6 +44,7 @@ const InfoScreen = props => {
   const [overlay2, setoverlay2] = useState(false);
   const [overlay3, setoverlay3] = useState(false);
   const [overlay4, setoverlay4] = useState(false);
+  const [timeOverlay, settimeOverlay] = useState(false);
   const [addr, setaddr] = useState('高譚市');
   const [addr2, setaddr2] = useState('二十一區');
   const [addr3, setaddr3] = useState('');
@@ -51,38 +52,105 @@ const InfoScreen = props => {
   const [email, setemail] = useState('');
   const [tel, settel] = useState('');
   const [pwd, setpwd] = useState('');
+  const [weekDay, setweekDay] = useState(1);
+  const [startHour, setstartHour] = useState(10);
+  const [endHour, setendHour] = useState(23);
 
   let testStr = profile?.ServiceArea;
   testStr = testStr?.split(',');
 
   let MondayServiceArr = profile?.MondayService;
   MondayServiceArr = MondayServiceArr?.split(',');
-  let MondayServiceText = (MondayServiceArr.length>1)?`${MondayServiceArr[0]}:00~${MondayServiceArr[MondayServiceArr.length-1]}:00`:'無';
+  let MondayServiceText =
+    MondayServiceArr?.length > 1
+      ? `${MondayServiceArr[0]}:00~${
+          MondayServiceArr[MondayServiceArr?.length - 1]
+        }:00`
+      : '無';
   let TuesdayServiceArr = profile?.TuesdayService;
   TuesdayServiceArr = TuesdayServiceArr?.split(',');
-  let TuesdayServiceText = (TuesdayServiceArr.length>1)?`${TuesdayServiceArr[0]}:00~${TuesdayServiceArr[TuesdayServiceArr.length-1]}:00`:'無';
+  let TuesdayServiceText =
+    TuesdayServiceArr?.length > 1
+      ? `${TuesdayServiceArr[0]}:00~${
+          TuesdayServiceArr[TuesdayServiceArr?.length - 1]
+        }:00`
+      : '無';
   let WednesdayServiceArr = profile?.WednesdayService;
   WednesdayServiceArr = WednesdayServiceArr?.split(',');
-  let WednesdayServiceText = (WednesdayServiceArr.length>1)?`${WednesdayServiceArr[0]}:00~${WednesdayServiceArr[WednesdayServiceArr.length-1]}:00`:'無';
+  let WednesdayServiceText =
+    WednesdayServiceArr?.length > 1
+      ? `${WednesdayServiceArr[0]}:00~${
+          WednesdayServiceArr[WednesdayServiceArr?.length - 1]
+        }:00`
+      : '無';
   let ThursdayServiceArr = profile?.ThursdayService;
   ThursdayServiceArr = ThursdayServiceArr?.split(',');
-  let ThursdayServiceText = (ThursdayServiceArr.length>1)?`${ThursdayServiceArr[0]}:00~${ThursdayServiceArr[ThursdayServiceArr.length-1]}:00`:'無';
+  let ThursdayServiceText =
+    ThursdayServiceArr?.length > 1
+      ? `${ThursdayServiceArr[0]}:00~${
+          ThursdayServiceArr[ThursdayServiceArr?.length - 1]
+        }:00`
+      : '無';
   let FridayServiceArr = profile?.FridayService;
   FridayServiceArr = FridayServiceArr?.split(',');
-  let FridayServiceText = (FridayServiceArr.length>1)?`${FridayServiceArr[0]}:00~${FridayServiceArr[FridayServiceArr.length-1]}:00`:'無';
+  let FridayServiceText =
+    FridayServiceArr?.length > 1
+      ? `${FridayServiceArr[0]}:00~${
+          FridayServiceArr[FridayServiceArr?.length - 1]
+        }:00`
+      : '無';
   let SaturdayServiceArr = profile?.SaturdayService;
   SaturdayServiceArr = SaturdayServiceArr?.split(',');
-  let SaturdayServiceText = (SaturdayServiceArr.length>1)?`${SaturdayServiceArr[0]}:00~${SaturdayServiceArr[SaturdayServiceArr.length-1]}:00`:'無';
+  let SaturdayServiceText =
+    SaturdayServiceArr?.length > 1
+      ? `${SaturdayServiceArr[0]}:00~${
+          SaturdayServiceArr[SaturdayServiceArr?.length - 1]
+        }:00`
+      : '無';
   let SundayServiceArr = profile?.SundayService;
   SundayServiceArr = SundayServiceArr?.split(',');
-  let SundayServiceText = (SundayServiceArr.length>1)?`${SundayServiceArr[0]}:00~${SundayServiceArr[SundayServiceArr.length-1]}:00`:'無';
-  
+  let SundayServiceText =
+    SundayServiceArr?.length > 1
+      ? `${SundayServiceArr[0]}:00~${
+          SundayServiceArr[SundayServiceArr?.length - 1]
+        }:00`
+      : '無';
 
-  console.log('TEST', MondayServiceArr,TuesdayServiceArr,WednesdayServiceArr,ThursdayServiceArr
-    ,FridayServiceArr,SaturdayServiceArr,SundayServiceArr);
-  console.log('TEST TEXT', MondayServiceText,TuesdayServiceText,WednesdayServiceText,ThursdayServiceText
-    ,FridayServiceText,SaturdayServiceText,SundayServiceText);
+  console.log(
+    'TEST',
+    MondayServiceArr,
+    TuesdayServiceArr,
+    WednesdayServiceArr,
+    ThursdayServiceArr,
+    FridayServiceArr,
+    SaturdayServiceArr,
+    SundayServiceArr,
+  );
+  console.log(
+    'TEST TEXT',
+    MondayServiceText,
+    TuesdayServiceText,
+    WednesdayServiceText,
+    ThursdayServiceText,
+    FridayServiceText,
+    SaturdayServiceText,
+    SundayServiceText,
+  );
+
+  let rows = [];
+  rows.push(<Picker.Item label={`無服務時間`} value={0} />);
+  for (let i = 10; i < 23; i++) {
+    rows.push(<Picker.Item label={`${i}:00`} value={i} />);
+  }
+  let rows2 = [];
+  rows2.push(<Picker.Item label={`無服務時間`} value={0} />);
+  for (let i = startHour + 1; i < 24; i++) {
+    rows2.push(<Picker.Item label={`${i}:00`} value={i} />);
+  }
+  console.log(startHour, endHour);
+
   let counties = Object.keys(City_counties);
+
   //console.log('TEST2', counties);
   //console.log('TEST3', counties2);
 
@@ -270,6 +338,76 @@ const InfoScreen = props => {
     setoverlay3(false);
   }
 
+  async function submitServiceTime(day) {
+    //console.log(tel);
+
+    let token = await getToken();
+    console.log('TOKEN???????????', token);
+    let serviceArr = [];
+    if (startHour !== 0 && endHour !== 0) {
+      for (let i = startHour; i <= endHour; i++) {
+        serviceArr.push(i);
+      }
+    }
+
+    serviceArr = serviceArr.toString();
+    console.log(serviceArr);
+    let temp = profile;
+    switch (day) {
+      default:
+        break;
+      case 1:
+        temp.MondayService = serviceArr;
+        break;
+      case 2:
+        temp.TuesdayService = serviceArr;
+        break;
+      case 3:
+        temp.WednesdayService = serviceArr;
+        break;
+      case 4:
+        temp.ThursdayService = serviceArr;
+        break;
+      case 5:
+        temp.FridayService = serviceArr;
+        break;
+      case 6:
+        temp.SaturdayService = serviceArr;
+        break;
+      case 7:
+        temp.SundayService = serviceArr;
+        break;
+    }
+
+    let url = `http://aso.1966.org.tw:20020/api/FootMaster/Put`;
+    const data = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(temp),
+    })
+      .then(response => response.json())
+      .then(res => {
+        if (res?.success) {
+          console.log('PUT MASTER AJAX', res);
+        } else {
+          console.log('TASK AJAX GGWP', res);
+          Alert.alert(res?.msg, ' ', [
+            {
+              text: '確定',
+              onPress: () => {},
+            },
+          ]);
+        }
+      })
+      .catch(err => {
+        console.log('fetchData_test', err);
+      });
+    settimeOverlay(false);
+  }
+
   async function submitPwd() {
     let url = `http://aso.1966.org.tw:20020/api/Login/PutForgetPasswordMaster?cAccount=${
       props?.route?.params?.addAcc
@@ -395,6 +533,78 @@ const InfoScreen = props => {
             title="確認送出"
             type="outline"
             onPress={() => submitAddr()}
+          />
+        </Overlay>
+
+        <Overlay
+          onBackdropPress={() => settimeOverlay(false)}
+          isVisible={timeOverlay}
+          windowBackgroundColor="rgba(255, 255, 255, .5)"
+          overlayBackgroundColor="white"
+          width="90%"
+          height="auto">
+          <Text
+            style={{
+              backgroundColor: '#964F19',
+              fontSize: 20,
+              fontWeight: 'bold',
+              color: 'white',
+              textAlign: 'center',
+              padding: 10,
+              marginBottom: 10,
+            }}>
+            {`修改服務時間`}
+          </Text>
+
+          <Picker
+            enabled={true}
+            selectedValue={weekDay}
+            onValueChange={(itemValue, itemIndex) => {
+              setweekDay(itemValue);
+              //console.log(itemValue);
+            }}>
+            <Picker.Item label="星期一" value={1} />
+            <Picker.Item label="星期二" value={2} />
+            <Picker.Item label="星期三" value={3} />
+            <Picker.Item label="星期四" value={4} />
+            <Picker.Item label="星期五" value={5} />
+            <Picker.Item label="星期六" value={6} />
+            <Picker.Item label="星期日" value={7} />
+          </Picker>
+
+          <Picker
+            enabled={true}
+            style={endHour === 0 && {display: 'none'}}
+            selectedValue={startHour}
+            onValueChange={(itemValue, itemIndex) => setstartHour(itemValue)}>
+            {rows}
+          </Picker>
+          {(endHour !== 0 && startHour !== 0) &&<Text style={{alignSelf: 'center'}}>至</Text>}
+          <Picker
+            enabled={true}
+            style={startHour === 0 && {display: 'none'}}
+            selectedValue={endHour}
+            onValueChange={(itemValue2, itemIndex2) => setendHour(itemValue2)}>
+            {rows2}
+          </Picker>
+
+          <Button
+            titleStyle={{
+              color: 'black',
+              fontWeight: 'normal',
+              fontSize: 20,
+            }}
+            containerStyle={{marginTop: 12}}
+            buttonStyle={{
+              alignSelf: 'flex-end',
+              borderColor: '#964F19',
+            }}
+            title="確認送出"
+            type="outline"
+            onPress={() => {
+              console.log(weekDay, startHour, endHour);
+              submitServiceTime(weekDay);
+            }}
           />
         </Overlay>
 
@@ -722,8 +932,245 @@ const InfoScreen = props => {
           <View style={{flexDirection: 'row'}}>
             <Text style={{flex: 1, color: '#999999'}}>服務時間</Text>
           </View>
-          <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
-            <Text>週一</Text>
+          <View>
+            <Button
+              onPress={() => {
+                settimeOverlay(true);
+                setweekDay(1);
+              }}
+              titleStyle={{
+                color: 'black',
+                fontWeight: 'normal',
+                fontSize: 14,
+                textAlign: 'left',
+              }}
+              containerStyle={{flex: 2, paddingLeft: 16, margin: 0}}
+              buttonStyle={{
+                padding: 0,
+                margin: 0,
+                alignSelf: 'flex-start',
+                flexWrap: 'wrap',
+                justifyContent: 'flex-start',
+              }}
+              title={`週一： ${MondayServiceText}`}
+              type="clear"
+              icon={
+                <Icon
+                  name="pencil"
+                  type="material-community"
+                  color="#666666"
+                  onPress={() => {
+                    settimeOverlay(true);
+                    setweekDay(1);
+                  }}
+                />
+              }
+              iconRight
+            />
+            <Button
+              onPress={() => {
+                settimeOverlay(true);
+                setweekDay(2);
+              }}
+              titleStyle={{
+                color: 'black',
+                fontWeight: 'normal',
+                fontSize: 14,
+                textAlign: 'left',
+              }}
+              containerStyle={{flex: 2, paddingLeft: 16, margin: 0}}
+              buttonStyle={{
+                padding: 0,
+                margin: 0,
+                alignSelf: 'flex-start',
+                flexWrap: 'wrap',
+                justifyContent: 'flex-start',
+              }}
+              title={`週二： ${TuesdayServiceText}`}
+              type="clear"
+              icon={
+                <Icon
+                  name="pencil"
+                  type="material-community"
+                  color="#666666"
+                  onPress={() => {
+                    settimeOverlay(true);
+                    setweekDay(2);
+                  }}
+                />
+              }
+              iconRight
+            />
+            <Button
+              onPress={() => {
+                settimeOverlay(true);
+                setweekDay(3);
+              }}
+              titleStyle={{
+                color: 'black',
+                fontWeight: 'normal',
+                fontSize: 14,
+                textAlign: 'left',
+              }}
+              containerStyle={{flex: 2, paddingLeft: 16, margin: 0}}
+              buttonStyle={{
+                padding: 0,
+                margin: 0,
+                alignSelf: 'flex-start',
+                flexWrap: 'wrap',
+                justifyContent: 'flex-start',
+              }}
+              title={`週三： ${WednesdayServiceText}`}
+              type="clear"
+              icon={
+                <Icon
+                  name="pencil"
+                  type="material-community"
+                  color="#666666"
+                  onPress={() => {
+                    settimeOverlay(true);
+                    setweekDay(3);
+                  }}
+                />
+              }
+              iconRight
+            />
+            <Button
+              onPress={() => {
+                settimeOverlay(true);
+                setweekDay(4);
+              }}
+              titleStyle={{
+                color: 'black',
+                fontWeight: 'normal',
+                fontSize: 14,
+                textAlign: 'left',
+              }}
+              containerStyle={{flex: 2, paddingLeft: 16, margin: 0}}
+              buttonStyle={{
+                padding: 0,
+                margin: 0,
+                alignSelf: 'flex-start',
+                flexWrap: 'wrap',
+                justifyContent: 'flex-start',
+              }}
+              title={`週四： ${ThursdayServiceText}`}
+              type="clear"
+              icon={
+                <Icon
+                  name="pencil"
+                  type="material-community"
+                  color="#666666"
+                  onPress={() => {
+                    settimeOverlay(true);
+                    setweekDay(4);
+                  }}
+                />
+              }
+              iconRight
+            />
+            <Button
+              onPress={() => {
+                settimeOverlay(true);
+                setweekDay(5);
+              }}
+              titleStyle={{
+                color: 'black',
+                fontWeight: 'normal',
+                fontSize: 14,
+                textAlign: 'left',
+              }}
+              containerStyle={{flex: 2, paddingLeft: 16, margin: 0}}
+              buttonStyle={{
+                padding: 0,
+                margin: 0,
+                alignSelf: 'flex-start',
+                flexWrap: 'wrap',
+                justifyContent: 'flex-start',
+              }}
+              title={`週五： ${FridayServiceText}`}
+              type="clear"
+              icon={
+                <Icon
+                  name="pencil"
+                  type="material-community"
+                  color="#666666"
+                  onPress={() => {
+                    settimeOverlay(true);
+                    setweekDay(5);
+                  }}
+                />
+              }
+              iconRight
+            />
+            <Button
+              onPress={() => {
+                settimeOverlay(true);
+                setweekDay(6);
+              }}
+              titleStyle={{
+                color: 'black',
+                fontWeight: 'normal',
+                fontSize: 14,
+                textAlign: 'left',
+              }}
+              containerStyle={{flex: 2, paddingLeft: 16, margin: 0}}
+              buttonStyle={{
+                padding: 0,
+                margin: 0,
+                alignSelf: 'flex-start',
+                flexWrap: 'wrap',
+                justifyContent: 'flex-start',
+              }}
+              title={`週六： ${SaturdayServiceText}`}
+              type="clear"
+              icon={
+                <Icon
+                  name="pencil"
+                  type="material-community"
+                  color="#666666"
+                  onPress={() => {
+                    settimeOverlay(true);
+                    setweekDay(6);
+                  }}
+                />
+              }
+              iconRight
+            />
+            <Button
+              onPress={() => {
+                settimeOverlay(true);
+                setweekDay(7);
+              }}
+              titleStyle={{
+                color: 'black',
+                fontWeight: 'normal',
+                fontSize: 14,
+                textAlign: 'left',
+              }}
+              containerStyle={{flex: 2, paddingLeft: 16, margin: 0}}
+              buttonStyle={{
+                padding: 0,
+                margin: 0,
+                alignSelf: 'flex-start',
+                flexWrap: 'wrap',
+                justifyContent: 'flex-start',
+              }}
+              title={`週日： ${SundayServiceText}`}
+              type="clear"
+              icon={
+                <Icon
+                  name="pencil"
+                  type="material-community"
+                  color="#666666"
+                  onPress={() => {
+                    settimeOverlay(true);
+                    setweekDay(7);
+                  }}
+                />
+              }
+              iconRight
+            />
           </View>
         </View>
         <View style={{borderWidth: 0.5, borderColor: '#DDDDDD'}} />
@@ -746,14 +1193,13 @@ const InfoScreen = props => {
             type="outline"
             onPress={() => {
               Alert.alert('確定登出?', ' ', [
-        {
-          text: '取消',
-          onPress: () => null,
-          style: 'cancel',
-        },
-        {text: '確定', onPress: () =>  handleLogout()},
-      ]);
-              
+                {
+                  text: '取消',
+                  onPress: () => null,
+                  style: 'cancel',
+                },
+                {text: '確定', onPress: () => handleLogout()},
+              ]);
             }}
           />
         </View>
